9eb90570b56478c3c50672156b0fb0e2cec17ba5
Alexey Venderov <alexey.venderov@getmoss.com>
2021-04-18 17:36:37 +0200

    first commit

---
+++ src/test/kotlin/com/github/avenderov/ApplicationTest.kt
@@ -0,0 +1,12 @@
+package com.github.avenderov
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    fun `context loads`() {
+    }
+}
